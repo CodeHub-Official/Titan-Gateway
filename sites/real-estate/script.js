44f03c3077765914c411f752b38324a39621a3bb
CodeHub-Titan <your-email@example.com>
2026-01-26 20:43:06 +0200

    Titan core backup without junk

--- sites/real-estate/script.js
+++
@@ -1,5 +0,0 @@
-function contact(item) {
-    const wa = "201012345678"; // Update your number
-    const url = "https://wa.me/" + wa + "?text=I am interested in acquiring the " + item;
-    window.open(url, '_blank');
-}
